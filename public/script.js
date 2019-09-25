d822a9a3edeeffe8dc9cee06f0a70e4d60d0e526
Per-Niklas Longberg <perniklaslongberg@gmail.com>
2019-09-25 22:40:25 +0200

    Broke the thing. Async is hard

--- public/script.js
+++ public/script.js
@@ -1,13 +1,13 @@
+var db,
+    soonest = 2569600000000;
+
 $(function() {
-    let timer = countdown(new Date('2019-12-31').getTime());
-    db.collection("timers").get().then((snapshot) => {
-        snapshot.forEach((doc) => {
-            console.log(doc.data());
-        });
-    });
+    db = firebase.firestore();
+    let timers = fetchAllTimers();
+    let timer = startCountdown(soonest);
 });
 
-function countdown(end) {
+function startCountdown(end) {
     return setInterval(function() {
         let now = new Date().getTime();
         timeBetween = end - now;
@@ -27,4 +27,18 @@ function countdown(end) {
             $('#seconds').text("0");
         }
     }, 1000);
+}
+
+function fetchAllTimers() {
+    let timers = [];
+    db.collection("timers").get().then((snapshot) => {
+        snapshot.forEach((doc) => {
+            timers.push(doc.data());
+            console.log(doc.data());
+            if (doc.data().end.seconds * 100 < soonest) {
+                soonest = doc.data().end.seconds * 100;
+            }
+        });
+    });
+    return timers;
 }
